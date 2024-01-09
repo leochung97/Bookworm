7b2c0d33dd52f46a06b097a7122c0e3f823cbb2e
Leo Chung <leo.chung97@gmail.com>
2024-01-09 11:23:04 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Leo Chung on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
